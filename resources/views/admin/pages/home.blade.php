c8c6424a5d3b37d79b713d85fc263194c2b73532
mdrefathossen <refathossen2001@gmail.com>
2024-08-15 22:04:49 +0600

    basic setup done

---
+++ resources/views/admin/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.master')
+
+@section('content')
+
+<h1>Hello home</h1>
+
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',[HomeController::class,'home']);
+Route::get('/category/list',[CategoryController::class,'list']);
+Route::get('/brand/list',[BrandController::class,'list']);
